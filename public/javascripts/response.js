9a4a8e718b83dc73379035a7df377b1a09cada01
Alex Votry <alexvotry@gmail.com>
2016-11-10 11:30:44 -0800

    added a picture to the README

--- public/javascripts/response.js
+++ public/javascripts/response.js
@@ -1,27 +1,4 @@
-// var xmlHttp = createXmlHttpRequestObject();
-//
-// function createXmlHttpRequestObject() {
-//   var xmlHttp;
-//
-//   if(window.ActiveXObject) {
-//     try{
-//       xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
-//     } catch(e) {
-//       xmlHttp = false;
-//     }
-//   } else {
-//     try {
-//       xmlHttp = new XMLHttpRequest();
-//     } catch(e) {
-//       xmlHttp = false;
-//     }
-//   }
-//   if(!xmlHttp)
-//     alert('no xmlHttp');
-//     else {
-//       return xmlHttp;
-//     }
-// }
+
 
 $(function() {
   $('#title').on('keydown', function(e) {
